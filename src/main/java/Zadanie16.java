1592cd1d3f1d25b3be2c669603b11f69353cf8a7
Michal <waldmanmichal@gmai.com>
2021-03-06 11:16:13 +0100

    Zadanie 16 poprawione

--- src/main/java/Zadanie16.java
+++ src/main/java/Zadanie16.java
@@ -7,15 +7,23 @@ public class Zadanie16 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input 10 integers: ");
         int lastNumber = 0;
-        int count = 0;
+        int count = 1;
+        int maxCount = 0;
 
         for (int i = 0; i < 10; i++) {
             int number = scanner.nextInt();
             if (number > lastNumber){
                 count += 1;
+                if (maxCount < count){
+                    maxCount = count;
+                }
+            }
+            else {
+                count = 1;
             }
             lastNumber = number;
         }
-        System.out.println(count);
+
+        System.out.println(maxCount);
     }
 }
